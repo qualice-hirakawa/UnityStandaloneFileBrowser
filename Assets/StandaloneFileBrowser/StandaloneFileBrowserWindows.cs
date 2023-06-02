4e7ac0c384b2486d3b05ca13d12c4d1455695395
Yuichiro Hirakawa <hirakawa@qualice.co.jp>
2023-06-02 13:37:00 +0900

    StandaloneFileBrowser.asmdef

--- Assets/StandaloneFileBrowser/StandaloneFileBrowserWindows.cs
+++ Assets/StandaloneFileBrowser/StandaloneFileBrowserWindows.cs
@@ -2,116 +2,158 @@
 
 using System;
 using System.IO;
+using System.Text;
+using System.Linq;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
-using Ookii.Dialogs;
+using Win32API.ShObjIdl_core;
+using ShObjIdl_core.WrappedFileDialog;
 
-namespace SFB {
+namespace SFB
+{
     // For fullscreen support
     // - WindowWrapper class and GetActiveWindow() are required for modal file dialog.
     // - "PlayerSettings/Visible In Background" should be enabled, otherwise when file dialog opened app window minimizes automatically.
 
-    public class WindowWrapper : IWin32Window {
+    public class WindowWrapper : IWin32Window
+    {
         private IntPtr _hwnd;
         public WindowWrapper(IntPtr handle) { _hwnd = handle; }
         public IntPtr Handle { get { return _hwnd; } }
     }
 
-    public class StandaloneFileBrowserWindows : IStandaloneFileBrowser {
+    public class StandaloneFileBrowserWindows : IStandaloneFileBrowser
+    {
         [DllImport("user32.dll")]
         private static extern IntPtr GetActiveWindow();
 
-        public string[] OpenFilePanel(string title, string directory, ExtensionFilter[] extensions, bool multiselect) {
-            var fd = new VistaOpenFileDialog();
-            fd.Title = title;
-            if (extensions != null) {
-                fd.Filter = GetFilterFromFileExtensionList(extensions);
-                fd.FilterIndex = 1;
+        public string[] OpenFilePanel(string title, string directory, ExtensionFilter[] extensions, bool multiselect)
+        {
+            using (var fd = new FileOpenDialog())
+            {
+                fd.SetTitle(title);
+                if (extensions != null)
+                {
+                    var specs = new COMDLG_FILTERSPEC[extensions.Length];
+                    for (int i = 0; i < extensions.Length; i++)
+                    {
+                        specs[i].pszName = extensions[i].Name;
+                        var size = extensions[i].Extensions.Sum((element) => element.Length) + extensions[i].Extensions.Length * 3;
+                        var builder = new StringBuilder(size);
+                        foreach (var ext in extensions[i].Extensions)
+                        {
+                            builder.Append("*.").Append(ext).Append(';');
+                        }
+                        builder.Remove(builder.Length - 1, 1);
+                        specs[i].pszSpec = builder.ToString();
+                    }
+                    fd.SetFileTypes(specs);
+                }
+                var fos = FOS.NOCHANGEDIR;
+                if (multiselect)
+                {
+                    fos |= FOS.ALLOWMULTISELECT;
+                }
+                fd.SetOptions(fos);
+                return fd.Show();
             }
-            else {
-                fd.Filter = string.Empty;
-            }
-            fd.Multiselect = multiselect;
-            if (!string.IsNullOrEmpty(directory)) {
-                fd.FileName = GetDirectoryPath(directory);
-            }
-            var res = fd.ShowDialog(new WindowWrapper(GetActiveWindow()));
-            var filenames = res == DialogResult.OK ? fd.FileNames : new string[0];
-            fd.Dispose();
-            return filenames;
+            //if (!string.IsNullOrEmpty(directory))
+            //{
+            //    fd.FileName = GetDirectoryPath(directory);
+            //}
+            //var res = fd.ShowDialog(new WindowWrapper(GetActiveWindow()));
+            //var filenames = res == DialogResult.OK ? fd.FileNames : new string[0];
+            //fd.Dispose();
+            //return filenames;
         }
 
-        public void OpenFilePanelAsync(string title, string directory, ExtensionFilter[] extensions, bool multiselect, Action<string[]> cb) {
+        public void OpenFilePanelAsync(string title, string directory, ExtensionFilter[] extensions, bool multiselect, Action<string[]> cb)
+        {
             cb.Invoke(OpenFilePanel(title, directory, extensions, multiselect));
         }
 
-        public string[] OpenFolderPanel(string title, string directory, bool multiselect) {
-            var fd = new VistaFolderBrowserDialog();
-            fd.Description = title;
-            if (!string.IsNullOrEmpty(directory)) {
-                fd.SelectedPath = GetDirectoryPath(directory);
-            }
-            var res = fd.ShowDialog(new WindowWrapper(GetActiveWindow()));
-            var filenames = res == DialogResult.OK ? new []{ fd.SelectedPath } : new string[0];
-            fd.Dispose();
-            return filenames;
+        public string[] OpenFolderPanel(string title, string directory, bool multiselect)
+        {
+            //var fd = new VistaFolderBrowserDialog();
+            //fd.Description = title;
+            //if (!string.IsNullOrEmpty(directory))
+            //{
+            //    fd.SelectedPath = GetDirectoryPath(directory);
+            //}
+            //var res = fd.ShowDialog(new WindowWrapper(GetActiveWindow()));
+            //var filenames = res == DialogResult.OK ? new[] { fd.SelectedPath } : new string[0];
+            //fd.Dispose();
+            //return filenames;
+            return new string[0];
         }
 
-        public void OpenFolderPanelAsync(string title, string directory, bool multiselect, Action<string[]> cb) {
+        public void OpenFolderPanelAsync(string title, string directory, bool multiselect, Action<string[]> cb)
+        {
             cb.Invoke(OpenFolderPanel(title, directory, multiselect));
         }
 
-        public string SaveFilePanel(string title, string directory, string defaultName, ExtensionFilter[] extensions) {
-            var fd = new VistaSaveFileDialog();
-            fd.Title = title;
+        public string SaveFilePanel(string title, string directory, string defaultName, ExtensionFilter[] extensions)
+        {
+            //var fd = new VistaSaveFileDialog();
+            //fd.Title = title;
 
-            var finalFilename = "";
+            //var finalFilename = "";
 
-            if (!string.IsNullOrEmpty(directory)) {
-                finalFilename = GetDirectoryPath(directory);
-            }
+            //if (!string.IsNullOrEmpty(directory))
+            //{
+            //    finalFilename = GetDirectoryPath(directory);
+            //}
 
-            if (!string.IsNullOrEmpty(defaultName)) {
-                finalFilename += defaultName;
-            }
+            //if (!string.IsNullOrEmpty(defaultName))
+            //{
+            //    finalFilename += defaultName;
+            //}
 
-            fd.FileName = finalFilename;
-            if (extensions != null) {
-                fd.Filter = GetFilterFromFileExtensionList(extensions);
-                fd.FilterIndex = 1;
-                fd.DefaultExt = extensions[0].Extensions[0];
-                fd.AddExtension = true;
-            }
-            else {
-                fd.DefaultExt = string.Empty;
-                fd.Filter = string.Empty;
-                fd.AddExtension = false;
-            }
-            var res = fd.ShowDialog(new WindowWrapper(GetActiveWindow()));
-            var filename = res == DialogResult.OK ? fd.FileName : "";
-            fd.Dispose();
-            return filename;
+            //fd.FileName = finalFilename;
+            //if (extensions != null)
+            //{
+            //    fd.Filter = GetFilterFromFileExtensionList(extensions);
+            //    fd.FilterIndex = 1;
+            //    fd.DefaultExt = extensions[0].Extensions[0];
+            //    fd.AddExtension = true;
+            //}
+            //else
+            //{
+            //    fd.DefaultExt = string.Empty;
+            //    fd.Filter = string.Empty;
+            //    fd.AddExtension = false;
+            //}
+            //var res = fd.ShowDialog(new WindowWrapper(GetActiveWindow()));
+            //var filename = res == DialogResult.OK ? fd.FileName : "";
+            //fd.Dispose();
+            //return filename;
+            return string.Empty;
         }
 
-        public void SaveFilePanelAsync(string title, string directory, string defaultName, ExtensionFilter[] extensions, Action<string> cb) {
+        public void SaveFilePanelAsync(string title, string directory, string defaultName, ExtensionFilter[] extensions, Action<string> cb)
+        {
             cb.Invoke(SaveFilePanel(title, directory, defaultName, extensions));
         }
 
         // .NET Framework FileDialog Filter format
         // https://msdn.microsoft.com/en-us/library/microsoft.win32.filedialog.filter
-        private static string GetFilterFromFileExtensionList(ExtensionFilter[] extensions) {
+        private static string GetFilterFromFileExtensionList(ExtensionFilter[] extensions)
+        {
             var filterString = "";
-            foreach (var filter in extensions) {
+            foreach (var filter in extensions)
+            {
                 filterString += filter.Name + "(";
 
-                foreach (var ext in filter.Extensions) {
+                foreach (var ext in filter.Extensions)
+                {
                     filterString += "*." + ext + ",";
                 }
 
                 filterString = filterString.Remove(filterString.Length - 1);
                 filterString += ") |";
 
-                foreach (var ext in filter.Extensions) {
+                foreach (var ext in filter.Extensions)
+                {
                     filterString += "*." + ext + "; ";
                 }
 
@@ -121,12 +163,15 @@ namespace SFB {
             return filterString;
         }
 
-        private static string GetDirectoryPath(string directory) {
+        private static string GetDirectoryPath(string directory)
+        {
             var directoryPath = Path.GetFullPath(directory);
-            if (!directoryPath.EndsWith("\\")) {
+            if (!directoryPath.EndsWith("\\"))
+            {
                 directoryPath += "\\";
             }
-            if (Path.GetPathRoot(directoryPath) == directoryPath) {
+            if (Path.GetPathRoot(directoryPath) == directoryPath)
+            {
                 return directory;
             }
             return Path.GetDirectoryName(directoryPath) + Path.DirectorySeparatorChar;
